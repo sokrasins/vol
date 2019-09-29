1d234f90e0eb1d0325fa2de2820d815bb3dba31b
sokrasins <sokrasins@gmail.com>
2019-09-28 23:02:26 -0400

    Add SDK and project

---
+++ src/nrf/SDK15.3/examples/ble_peripheral/ble_app_blinky/pca10040/s132/armgcc/_build/nrf52832_xxaa/system_nrf52.c.d
@@ -0,0 +1,62 @@
+_build/nrf52832_xxaa/system_nrf52.c.o: \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.c \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stdint.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\stdint.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\machine\_default_types.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\features.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\_newlib_version.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_intsup.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_stdint.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stdbool.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stdint.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\stdint.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\machine\_default_types.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\features.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\_newlib_version.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_intsup.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_stdint.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stdbool.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_name_change.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
---
+++ src/nrf/SDK15.3/examples/ble_peripheral/ble_app_blinky/pca10040/s132/armgcc/_build/nrf52832_xxaa/utf.c.d
@@ -0,0 +1,29 @@
+_build/nrf52832_xxaa/utf.c.o: \
+ ../../../../../../external/utf_converter/utf.c \
+ ../../../../../../external/utf_converter/utf.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stddef.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stdint.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\stdint.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\machine\_default_types.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\features.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\_newlib_version.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_intsup.h \
+ d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_stdint.h
+
+../../../../../../external/utf_converter/utf.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stddef.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\lib\gcc\arm-none-eabi\8.3.1\include\stdint.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\stdint.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\machine\_default_types.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\features.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\_newlib_version.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_intsup.h:
+
+d:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\8\ 2019-q3-update\arm-none-eabi\include\sys\_stdint.h:
